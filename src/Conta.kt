9931a5f66ca3eec861d6b8d21db7631d5e458aaf
RogerRF <rochilson@gmail.com>
2021-03-05 13:19:25 -0300

    Add Classe Conta

---
+++ src/Conta.kt
@@ -0,0 +1,11 @@
+class Conta(
+    val agencia: String,
+    val numero : String,
+    val saldo  : Double ) {
+    fun deposito(valor: Double){
+
+    }
+    fun saque(valor: Double){
+
+    }
+}
--- src/Pessoa.kt
+++ src/Pessoa.kt
@@ -1,16 +1,18 @@
 class Pessoa {
     var nome: String = "Roger"
+    var cpf: String = "123.456.789.01"
+    private set
 
-    var cpf: String = "123.456.789.01"; private set
+    //constructor()
+
+    fun getData() = cpf +" - "+ nome
 }
 
 fun main () {
 
     val p = Pessoa()
-    p.nome = "rochison"
 
     println(p)
-    println(p.nome)
-    println(p.cpf)
+    println(p.getData())
 
 }
